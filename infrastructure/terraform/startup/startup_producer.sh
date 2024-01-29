92ae489b25ead963a37ae6a1783e4f7833ab2e87
valentin-carl <vacleantrinl@gmail.com>
2024-01-29 22:56:43 +0100

    startup script for producer (finally) works

---
+++ infrastructure/terraform/startup/startup_producer.sh
@@ -0,0 +1,26 @@
+exec > >(tee -a /var/log/startup.log) 2>&1
+
+echo "startup script executed"
+env
+curl neverssl.com # ensure internet connection through net works
+
+sudo apt-get update
+sudo apt-get install -y zsh ranger htop network-manager net-tools git vim curl iputils-ping
+
+wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/local/go/bin"' | sudo tee /etc/sudoers.d/spath
+
+mkdir benchmark
+cd benchmark
+
+git clone https://github.com/valentin-carl/cloud-service-benchmarking.git
+cd cloud-service-benchmarking/producer
+sudo go build ./cmd/main.go
+
+ls -lah
+echo done
