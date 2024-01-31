2de6646fe05fde901018e0613def1ca59ec475af
valentin-carl <vacleantrinl@gmail.com>
2024-01-31 22:19:35 +0100

    update start_experiment.sh

--- infrastructure/setup/start_experiment.sh
+++ infrastructure/setup/start_experiment.sh
@@ -1,11 +1,4 @@
-# todo idea for nodeid stuff: when starting, do "export \$NODEID=0; ./main"
-# at least this works:
-# `gcloud compute ssh producer-instance-0 --zone=europe-west10-a --command='export A=1; env'`
-# so the programs should be able to also see that environment variable??
-
-# todo quorum queue configure replica count
-# todo rabbitmq configure memory stuff 40% -> 70%
-
+#!/bin/sh
 
 N_PROD=$1
 N_CONS=$2
@@ -28,41 +21,18 @@ fi
 
 # start consumers
 for (( i = 0 ; i < "$N_CONS" ; i++ )); do
-    gcloud compute ssh "consumer-instance-$i" --command "cd /benchmark/cloud-service-benchmarking/consumer; export NODEID=$i; sudo -E \$NODEID bash -c './main'" & # TODO test
+    gcloud compute ssh "consumer-instance-$i" --command \
+		"export NODEID=$i; sudo -E bash -c '/benchmark/cloud-service-benchmarking/consumer/main &>/dev/null &'"
 done
 
 # start monitoring at broker nodes
 for (( i = 0 ; i < "$N_BROK" ; i++ )); do
-    gcloud compute ssh "broker-instance-$i" --command "cd /benchmark/cloud-service-benchmarking/monitoring; export NODEID=$i; sudo -E \$NODEID bash -c './main'" & # TODO test
+	gcloud compute ssh "broker-instance-$i" --command \
+		"export NODEID=$i; sudo -E bash -c '/benchmark/cloud-service-benchmarking/monitoring/main &>/dev/null &'"
 done
 
 # start producers
 for (( i = 0 ; i < "$N_PROD" ; i++ )); do
-    gcloud compute ssh "producer-instance-$i" --command "cd /benchmark/cloud-service-benchmarking/producer; sudo ./main'" & # TODO test
+    gcloud compute ssh "producer-instance-$i" --command \
+        "sudo -E bash -c '/benchmark/cloud-service-benchmarking/producer/main &>/dev/null &'"
 done
-
-# TODO check what happens if this script runs through? does that the processes running on the VMs? In that case, try moving the &s into the remote commands
-# TODO redirect the output of these programs to /var/log/... using tee?
-
-
-
-
-
-
-
-
-
-for (( i = 0 ; i < "$N_BROK" ; i++ )); do
-    gcloud compute ssh "broker-instance-$i" --command \
-        "cd /benchmark/cloud-service-benchmarking/monitoring; export NODEID=$i; sudo -E bash -c './main'" &
-done
-
-
-
-
-
-
-
-
-
-
